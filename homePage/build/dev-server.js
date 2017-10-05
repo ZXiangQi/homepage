8215ad15c8b1f2a28fc52db724a131868cf6f37e
xiangqi <1342592931@qq.com>
2017-10-05 23:43:08 +0800

    加上链接

--- homePage/build/dev-server.js
+++ homePage/build/dev-server.js
@@ -21,6 +21,14 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+// var linkData = require('../data.json')
+// var router = express.Router()
+// router.get('/link', function(req, res) {
+//   res.json(linkData)
+// })
+// app.use(router)
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
--- homePage/src/App.vue
+++ homePage/src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <v-header></v-header>
+    <div id="content">
+      <ul>
+        <li v-for="(item, title) in linkData" class="linkSection">
+          <h3 class="title">{{title}}</h3>
+          <ul class="linkList">
+            <li v-for="(link, key) in item">
+              <a :href="link" target="_blank" :title="key">{{key}}</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/header/header'
+import linkData from './data.json'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      linkData: linkData
+    }
+  },
   components: {
     'v-header': Header
   }
@@ -16,6 +34,50 @@ export default {
 </script>
 
 <style>
-#app {
-}
+  #app{
+    width: 100%;
+    height: 100%;
+    background: #eee;
+  }
+  #content{
+    width: 80%;
+    margin: 0 auto;
+    padding: 10px;
+  }
+  #content .linkSection{
+    margin: 10px 0 20px;
+    background: #fff;
+    box-shadow: 1px 2px 4px #999;
+  }
+  #content .linkSection .title{
+    height: 30px;
+    padding: 0 10px;
+    line-height: 30px;
+    background: #403E4E;
+    color: #fff;
+  }
+  #content .linkSection .linkList{
+    padding: 10px;
+  }
+  #content .linkSection .linkList li{
+    display: inline-block;
+  }
+  #content .linkSection .linkList li a{
+    display: block;
+    width: 100px;
+    height: 30px;
+    margin: 0 10px;
+    padding: 0 10px;
+    line-height: 30px;
+    border-radius: 5px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    text-align: center;
+    background: #DADCDB;
+  }
+  #content .linkSection .linkList li a:hover{
+    background: #E1E1E1;
+    color: #fff;
+  }
 </style>
